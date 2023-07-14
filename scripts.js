15da6dba13e61236732a736dea4a31b8e44cf265
erinmuzughi <erinmuzughi@gmail.com>
2023-07-13 20:29:53 -0500

    Studio 1 through 2c completed

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    let takeoffButton = document.getElementById('takeoff');
+    let flightStatus1 = document.getElementById('flightStatus');
+    let shuttleBackgroundColor = document.getElementById('shuttleBackground');
+    //function goes here
+    takeoffButton.addEventListener("click", function(event){
+        console.log("Confirm the shuttle is ready to takeoff.");
+        flightStatus1.innerHTML = 'Shuttle in flight.';
+        shuttleBackgroundColor.style.backgroundColor = "blue";
+    }); 
+    
+
+}     window.addEventListener('load', init)
+
